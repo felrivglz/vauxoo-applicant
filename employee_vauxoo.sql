0f2c91b4f16d8ce75460ac28b0c8db1f1648c5de
felrivglz <felrivglz@gmail.com>
2017-02-18 11:07:51 -0600

    sql2+sql1 part with comments and data (final version)

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -61,3 +61,52 @@ INSERT INTO employee (first_name, last_name, departament_id)
 INSERT INTO employee (first_name, last_name, departament_id)
   VALUES ( 'Jorge', 'Rivera', 2) ;
 --end sql1 part
+-- In this section created 2 tables.
+--the table employee_hobby contain information about hobys
+--start sql2 part
+CREATE TABLE employee_hobby (
+  id serial NOT NULL,
+  name VARCHAR(15) NOT NULL,
+  description  TEXT,
+  PRIMARY KEY (id),
+  UNIQUE (id, name)
+);
+--the table employees_whit_hobbys contain  retation  many(employees) to many(hobys)
+CREATE TABLE employees_whit_hobbys (
+  id_employee INT NOT NULL,
+  id_employee_hobby INT NOT NULL,
+  PRIMARY KEY (id_employee, id_employee_hobby),
+  CONSTRAINT id_employees_TO_id_table_employee FOREIGN KEY (id_employee)
+      REFERENCES employee (id),
+  CONSTRAINT id_employee_hobby_to_hoby FOREIGN KEY (id_employee)
+      REFERENCES employee (id)
+);
+--add 4 hobys in the table employee_hobby
+INSERT INTO employee_hobby (name, description)
+  VALUES ( 'Run', 'run 5k or run 10k') ;
+INSERT INTO employee_hobby (name, description)
+  VALUES ( 'Bike', 'bike 50k or bike 100k') ;
+INSERT INTO employee_hobby (name, description)
+  VALUES ( 'Dance', 'Dance 1/2h or Dance 1h') ;
+INSERT INTO employee_hobby (name, description)
+  VALUES ( 'Read', 'read 1/2h or read 1h') ;
+--add relation employees with hobys
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 1, 1);
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 1, 2);
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 2, 3);
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 2, 4);
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 3, 1);
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 3, 3);
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 4, 2);
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 4, 4);
+INSERT INTO employees_whit_hobbys (id_employee, id_employee_hobby)
+  VALUES ( 4, 3);
+--end sql2 part
