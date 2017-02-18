8d8f2ab2b9f4b8a2dc7bdc444a75a8c0dca6ea99
felrivglz <felrivglz@gmail.com>
2017-02-18 01:02:40 -0600

    part sql1

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -4,12 +4,57 @@
 --       Consider add ';' at end sentence.
 
 CREATE TABLE employee (
+  id SERIAL NOT NULL,
+  first_name VARCHAR(25) NOT NULL,
+  last_name VARCHAR(25) NOT NULL,
+  PRIMARY KEY (id),
+  UNIQUE(id)
 );
 
 CREATE TABLE employee_department (
+  id SERIAL NOT NULL,
+  name varchar(50) NOT NULL,
+  description TEXT,
+  PRIMARY KEY (id),
+  UNIQUE(id)
 );
 
-CREATE TABLE employee_hobby (
-);
+ALTER TABLE employee
+ADD COLUMN departament_id INT NOT NULL;
+
+ALTER TABLE employee
+ ADD CONSTRAINT DEPARTAMENTS
+ FOREIGN KEY (departament_id)
+ REFERENCES employee_department(id);
+
+INSERT INTO employee_department (name, description)
+VALUES ( 'SISTEMAS', 'computers');
+
+ INSERT INTO employee_department (name, description)
+ VALUES ( 'RH', 'computers departemet');
+
+ INSERT INTO employee_department (name, description)
+ VALUES ( 'SALES', 'computers departemet');
+
+ INSERT INTO employee_department (name, description)
+ VALUES ( 'CLEAN', 'computers departemet');
+
+INSERT INTO employee (first_name, last_name, departament)
+VALUES ( 'Rubén', 'Ramirez', 1) ;
+INSERT INTO employee (first_name, last_name, departament)
+VALUES ( 'Raul', 'Ruiz', 2) ;
+INSERT INTO employee (first_name, last_name, departament)
+VALUES ( 'Felipe', 'Rivera', 3 ) ;
+INSERT INTO employee (first_name, last_name, departament)
+VALUES ( 'Christian', 'Gonzalez', 4) ;
+INSERT INTO employee (first_name, last_name, departament)
+VALUES ( 'Sergio', 'Rivera', 2) ;
+INSERT INTO employee (first_name, last_name, departament)
+VALUES ( 'Jorge', 'Rivera', 4) ;
+
+
+--CREATE TABLE employee_hobby (
+
+--);
 
 -- ...
