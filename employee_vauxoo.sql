e126f4db43b1c3ca92edfb148419118716c67537
felrivglz <felrivglz@gmail.com>
2017-02-18 01:31:43 -0600

    part sql1 with comments

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -18,14 +18,17 @@ CREATE TABLE employee_department (
   PRIMARY KEY (id),
   UNIQUE(id)
 );
-
+-- add a new COLUMN recall departament_id.
 ALTER TABLE employee
 ADD COLUMN departament_id INT NOT NULL;
-
+--Create a match between "despertamet_id" of employee and
+--"id" of enployee_depertament
 ALTER TABLE employee
  ADD CONSTRAINT DEPARTAMENTS
  FOREIGN KEY (departament_id)
  REFERENCES employee_department(id);
+
+--insert 4 departaments
 
 INSERT INTO employee_department (name, description)
 VALUES ( 'SISTEMAS', 'computers');
@@ -38,18 +41,24 @@ VALUES ( 'SISTEMAS', 'computers');
 
  INSERT INTO employee_department (name, description)
  VALUES ( 'CLEAN', 'computers departemet');
+--insert 6 employee.
 
-INSERT INTO employee (first_name, last_name, departament)
+INSERT INTO employee (first_name, last_name, departament_id)
 VALUES ( 'Rubén', 'Ramirez', 1) ;
-INSERT INTO employee (first_name, last_name, departament)
+
+INSERT INTO employee (first_name, last_name, departament_id)
 VALUES ( 'Raul', 'Ruiz', 2) ;
-INSERT INTO employee (first_name, last_name, departament)
+
+INSERT INTO employee (first_name, last_name, departament_id)
 VALUES ( 'Felipe', 'Rivera', 3 ) ;
-INSERT INTO employee (first_name, last_name, departament)
+
+INSERT INTO employee (first_name, last_name, departament_id)
 VALUES ( 'Christian', 'Gonzalez', 4) ;
-INSERT INTO employee (first_name, last_name, departament)
+
+INSERT INTO employee (first_name, last_name, departament_id)
 VALUES ( 'Sergio', 'Rivera', 2) ;
-INSERT INTO employee (first_name, last_name, departament)
+
+INSERT INTO employee (first_name, last_name, departament_id)
 VALUES ( 'Jorge', 'Rivera', 4) ;
 
 
