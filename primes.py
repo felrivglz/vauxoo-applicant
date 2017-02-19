078111daf419164ea561ee52bcd1787f84dac15b
FelipeRivera <felrivglz@gmail.com>
2017-02-19 05:51:33 -0600

    primes.py v1

--- primes.py
+++ primes.py
@@ -1,10 +1,10 @@
-®"""
+"""
 The module primes contain a class for the evaluation of intern
 vauxoo aplication
 """
 
 
-class prime_class(object):
+class PrimeClass(object):
     """
     This class contains one method called is_prime, this method
     requires an integer number for working
@@ -14,15 +14,16 @@ class prime_class(object):
         the is_prime method get a integer number and determine
         if number is primes
         """
-        for x in range(2, num_int):
-            if num_int % x == 0:
-                return False
-                break
+        cont = 0
+        if num_int != 0 and num_int != 1:
+            if num_int > 2:
+                for valor in range(2, num_int):
+                    if num_int % valor == 0:
+                        cont += 1
+                        return False
+                if cont == 0:
+                    return True
+            else:
+                return True
         else:
-            return True
-
-"""
-for check uncomment the next lines:
-g= prime_class();
-print g.is_prime(997)
-"""
+            return False
