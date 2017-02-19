4ec7d8e143b3a686be411f0f185e19e5f56c7e9c
FelipeRivera <felrivglz@gmail.com>
2017-02-19 05:55:00 -0600

    calculator .p v1

--- calculator.py
+++ calculator.py
@@ -4,7 +4,7 @@ for the evaluation of intern vauxoo aplication
 """
 
 
-class calculator_class(object):
+class CalculatorClass(object):
     """
     This class contains one method called sum, this method
     requires a list number for working
@@ -15,14 +15,8 @@ class calculator_class(object):
         its values, returned integer number with the sum
         """
         resultado = 0
-        x = len(num_list)
-        for n in num_list:
-            resultado += n
+        for numero in num_list:
+            resultado += numero
         return resultado
 
-"""
-for check uncomment the next lines:
-listaa = [1, 3, 4, 5, 6, 7, 8, 9]
-x = calculator_class()
-print x.sum(listaa)
-"""
+
